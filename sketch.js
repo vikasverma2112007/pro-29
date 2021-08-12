4b7907fecdb0c9d18cbfcbd7ea4b919cd6728dfa
vikasverma2112007 <88591854+vikasverma2112007@users.noreply.github.com>
2021-08-12 21:37:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -72,7 +72,7 @@ function draw() {
   imageMode(CENTER);
   // write image() to display the polygon image 
   //use the same x and y position as ball
-  
+  image(rock_img,ball.position.x,ball.position.y,40,40)
 
 
   stroke(0,0,0);
